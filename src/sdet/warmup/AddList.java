7c7a22d9b2ccf9ef3bebb2d653dfdbb466061370
ajayb95 <ajay95.balaji@gmail.com>
2020-09-20 18:27:09 +0530

    Warm up code upload

---
+++ src/sdet/warmup/AddList.java
@@ -0,0 +1,16 @@
+package sdet.warmup;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AddList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = "abccdddefghh";
+		List<String> li=Arrays.asList(input.split(""));
+		System.out.println(li);
+	}
+
+}
---
+++ src/sdet/warmup/MoveZeros.java
@@ -0,0 +1,23 @@
+package sdet.warmup;
+
+public class MoveZeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr={1, 2, 0, 4, 3, 0, 5, 0};
+		
+		int cnt=0;
+		
+		for(int i=0;i<arr.length;i++)
+			if(arr[i]!=0)
+				arr[cnt++]=arr[i];
+		
+		while(cnt<arr.length)
+			arr[cnt++]=0;
+		
+		for(int a:arr)
+			System.out.print(a+" ");
+	}
+
+}
