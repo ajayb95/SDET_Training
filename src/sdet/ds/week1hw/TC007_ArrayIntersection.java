c30511400fc1120f23ece988a9446a9d1187e5c4
e5617111 <e5617111@MAA-LT3T6YM13.FNFIS.com>
2020-09-24 14:13:17 +0530

    week1 homework

--- src/sdet/ds/week1hw/TC007_ArrayIntersection.java
+++ src/sdet/ds/week1hw/TC007_ArrayIntersection.java
@@ -1,5 +1,10 @@
 package sdet.ds.week1hw;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
 /*
 Given 3 integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, 
 return a sorted array of only the integers that appeared in all three arrays.
@@ -11,5 +16,29 @@ Explanation: Only 1 and 5 appeared in the 3 arrays.
 */
 
 public class TC007_ArrayIntersection {
+	@Test
+	public void example1() {
+		int[] arr1 = { 1, 2, 3, 4, 5 };
+		int[] arr2 = { 1, 2, 5, 7, 9 };
+		int[] arr3 = { 1, 3, 4, 5, 8 };
 
+		bruteForce_1(arr1, arr2, arr3);
+	}
+
+	private void bruteForce_1(int[] arr1, int[] arr2, int[] arr3) {
+		List<Integer> temp = new ArrayList<Integer>();
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr2.length; j++) {
+				if (arr1[i] == arr2[j])
+					temp.add(arr1[i]);
+			}
+		}
+
+		for (int i : temp) {
+			for (int j = 0; j < arr3.length; j++) {
+				if (i == arr3[j])
+					System.out.println(i);
+			}
+		}
+	}
 }
