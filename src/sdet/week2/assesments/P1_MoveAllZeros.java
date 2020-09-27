2c526fa06dd6cbd15586c9aace294ae47506575f
ajayb95 <ajay95.balaji@gmail.com>
2020-09-27 20:07:00 +0530

    Week2 changes

--- src/sdet/week2/assesments/P1_MoveAllZeros.java
+++ src/sdet/week2/assesments/P1_MoveAllZeros.java
@@ -1,5 +1,9 @@
 package sdet.week2.assesments;
 
+import java.util.Arrays;
+
+import org.junit.Test;
+
 /*
  Write a program to move all zeros to end
  Input: [1,0,5,9,0,0,6,8]
@@ -8,4 +12,46 @@ package sdet.week2.assesments;
 
 public class P1_MoveAllZeros {
 
+	@Test
+	public void example1() {
+		int[] input= {1,0,5,9,0,0,6,8};
+		moveZeros(input);
+	}
+	
+	@Test
+	public void example2() {
+		int[] input= {1,-5,-6,0,0,7,7,2,0,-1};
+		moveZeros(input);
+	}
+	
+	@Test
+	public void example3() {
+		int[] input= {1,2,3,6,5,4,7,8};
+		moveZeros(input);
+	}
+	
+	private void moveZeros(int[] input) {
+		int[] output=new int[input.length];
+		int count=0;
+		for (int i = 0; i < input.length; i++) {
+			if(input[i]!=0)
+				output[count++]=input[i];
+		}
+		System.out.println(Arrays.toString(output));
+	}
+	
+	private void moveZeros2(int[] input) {
+		int count=0;
+		for (int i = 0; i < input.length; i++) {
+			if(input[i]!=0)
+				input[count++]=input[i];
+		}
+		for (int i = count; i < input.length; i++) {
+			input[i]=0;
+		}
+	}
+	
+	private void moveZeros3(int[] input) {
+
+	}
 }
