c1ce29b6efcfd043999b1ab059168f08581f98df
e5617111 <e5617111@MAA-LT3T6YM13.FNFIS.com>
2020-10-28 19:41:36 +0530

    BT changes

---
+++ src/sdet/week6/ll_Q_stack/BinaryTreeTest.java
@@ -0,0 +1,29 @@
+package sdet.week6.ll_Q_stack;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import com.java.binarysearchtree.BinarySearchTree;
+import com.java.binarysearchtree.TreeTraversalOrder;
+
+public class BinaryTreeTest {
+
+	public static void main(String[] args) {
+		BinarySearchTree<Integer> bt=new BinarySearchTree<>();
+		
+		bt.add(4);
+		bt.add(2);
+		bt.add(5);
+		bt.add(1);
+		bt.add(3);
+		
+		Iterator<Integer> traverse = bt.traverse(TreeTraversalOrder.IN_ORDER);
+		int[] op=new int[bt.size()];
+		int i=0;
+		while(traverse.hasNext())
+			op[i++]=traverse.next();
+		
+		System.out.println(Arrays.toString(op));
+		
+	}
+}
