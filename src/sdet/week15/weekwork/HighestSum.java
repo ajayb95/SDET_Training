41db9991aa4cd70eeee5c1d5947ad9643ddd0772
e5617111 <e5617111@MAA-LT3T6YM13.FNFIS.com>
2020-12-22 19:17:16 +0530

    Changes

--- src/sdet/week15/weekwork/HighestSum.java
+++ src/sdet/week15/weekwork/HighestSum.java
@@ -1,31 +1,55 @@
 package sdet.week15.weekwork;
+//https://leetcode.com/problems/maximum-subarray/discuss/981146/Java-approach-Recursive-DP-Kadane-Divide-and-Conquer
+import java.util.Arrays;
 
 import org.junit.Test;
 
 public class HighestSum {
 
+	private int maxsum=Integer.MIN_VALUE;
+	
 	@Test
 	public void example1() {
 		int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(divideAndConquer(nums));
 		System.out.println(kandaneAlgorithm(nums));
 	}
 
 	@Test
 	public void example2() {
 		int[] nums = { -2, -3, 4, -1, -2, 1, 5, -3 };
-		System.out.println(kandaneAlgorithm(nums));
+		System.out.println(divideAndConquer(nums));
+		//System.out.println(kandaneAlgorithm(nums));
 	}
 
 	@Test
 	public void example3() {
 		int[] nums = { 1, -5, 2, -3, 7, 1 };
-		System.out.println(kandaneAlgorithm(nums));
+		System.out.println(divideAndConquer(nums));
+		//System.out.println(kandaneAlgorithm(nums));
 	}
 
 	@Test
 	public void example4() {
 		int[] nums = { -2, -1 };
-		System.out.println(kandaneAlgorithm(nums));
+		System.out.println(divideAndConquer(nums));
+		//System.out.println(kandaneAlgorithm(nums));
+	}
+	
+	private int divideAndConquer(int[] nums) {
+		return recursion(nums, 0, nums.length-1);
+	}
+	
+	private int recursion(int[] num,int l,int r) {
+		
+		if(l==r)
+			return num[l];
+		int mid=l+(r-l)/2;
+		int left=recursion(num, l, mid);
+		int right=recursion(num, mid+1, r);
+		
+		return Math.max(maxsum, left+right);
+	
 	}
 
 	private int kandaneAlgorithm(int[] nums) {
