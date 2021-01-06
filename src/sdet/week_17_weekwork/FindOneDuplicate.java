d05ab9fc1907a1c670750153d80e245b0143267b
ajayb95 <ajay95.balaji@gmail.com>
2021-01-06 22:19:20 +0530

    Changes

--- src/sdet/week_17_weekwork/FindOneDuplicate.java
+++ src/sdet/week_17_weekwork/FindOneDuplicate.java
@@ -14,43 +14,62 @@ public class FindOneDuplicate {
 	/*
 	 * https://leetcode.com/problems/find-the-duplicate-number/solution/
 	 */
-	
+
 	@Test
 	public void example1() {
-		int[] nums= {1,2,2,3,4};
-		System.out.println(findDuplicateSet(nums));
-		System.out.println(findDuplicateMap(nums));
+		int[] nums = { 1, 2, 2, 3, 4 };
+		//System.out.println(findDuplicateSet(nums));
+		//System.out.println(findDuplicateMap(nums));
+		System.out.println(floydCycle(nums));
 	}
-	
+
 	@Test
 	public void example2() {
-		int[] nums= {1,6,5,4,8,6};
-		System.out.println(findDuplicateSet(nums));
-		System.out.println(findDuplicateMap(nums));
+		int[] nums = { 2,5,9,6,9,3,8,9,7,1 };
+		//System.out.println(findDuplicateSet(nums));
+		//System.out.println(findDuplicateMap(nums));
+		System.out.println(floydCycle(nums));
 	}
-	
-	//Using set
-	//Add each element to set, if the element is present, return that element
+
+	private int floydCycle(int[] nums) {
+		// Required: space: o[1] and time:o[n]
+		int tortoise = nums[0];
+		int hare = nums[0];
+
+		do {
+			tortoise = nums[tortoise];
+			hare = nums[nums[hare]];
+		} while (tortoise != hare);
+		tortoise = nums[0];
+		while (tortoise != hare) {
+			tortoise = nums[tortoise];
+			hare = nums[hare];
+		}
+		return tortoise;
+	}
+
+	// Using set
+	// Add each element to set, if the element is present, return that element
 	private int findDuplicateSet(int[] nums) {
-		Set<Integer> set=new HashSet<>();
-		for(int each:nums)
-			if(!set.add(each))
+		Set<Integer> set = new HashSet<>();
+		for (int each : nums)
+			if (!set.add(each))
 				return each;
 		return -1;
 	}
-	
-	//Using Map by finding occurrences
+
+	// Using Map by finding occurrences
 	private int findDuplicateMap(int[] nums) {
-		Map<Integer, Integer> map=new HashMap<>();
-		for(int each:nums)
-			map.put(each, map.getOrDefault(each, 0)+1);
-		List<Integer> collect = map.entrySet().stream().filter(mp->mp.getValue()==2).flatMap(fm->Stream.of(fm.getKey())).collect(Collectors.toList());
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int each : nums)
+			map.put(each, map.getOrDefault(each, 0) + 1);
+		List<Integer> collect = map.entrySet().stream().filter(mp -> mp.getValue() == 2)
+				.flatMap(fm -> Stream.of(fm.getKey())).collect(Collectors.toList());
 		return collect.get(0);
 	}
-	
+
 	private void findDuplicate(int[] nums) {
-		//Required: space: o[1] and time:o[n]
-		
-		
+		// Required: space: o[1] and time:o[n]
+
 	}
 }
