9e593668aa5559bc01add54c536c4d4b2284726e
e5617111 <e5617111@MAA-LT3T6YM13.FNFIS.com>
2021-01-11 20:16:23 +0530

    Changes

--- src/sdet/week_18/weekwork/RecamansSequence.java
+++ src/sdet/week_18/weekwork/RecamansSequence.java
@@ -1,6 +1,34 @@
 package sdet.week_18.weekwork;
-/*https://www.geeksforgeeks.org/recamans-sequence/
-*/
-public class RecamansSequence {
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/*https://www.geeksforgeeks.org/recamans-sequence/*/
+public class RecamansSequence {
+//Rule:
+	/*
+	 * a(0) = 0, 
+	 * if n > 0 and the number is not already included in the sequence,
+	 	* a(n) = a(n - 1) - n 
+	 * else 
+	 	* a(n) = a(n-1) + n.
+	 */
+	
+public static void main(String[] args) {
+	int n = 6;
+	Set<Integer> set = new LinkedHashSet<Integer>();
+	set.add(0);
+	int cur = 0, prev = 0;
+	for (int i = 1; i < n; i++) {
+		cur = prev - i;
+		if (cur < 0 || set.contains(cur))
+			cur = prev + i;
+		set.add(cur);
+		prev = cur;
+	}
+
+	System.out.println(set);
+}
 }
