93d47794e29257cfac171fa69d21da7410b4ca16
e5617111 <e5617111@MAA-LT3T6YM13.FNFIS.com>
2021-01-19 20:39:00 +0530

    Changes

---
+++ src/sdet/week_19/weekwork/FindClosest.java
@@ -0,0 +1,59 @@
+package sdet.week_19.weekwork;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+import org.junit.Test;
+
+/*
+Find the closest element for each number
+
+Input: arr[]={2,1,5,8,3}
+Output: -1 2 2 5 2
+
+Explanation:
+[2], it is the only number in this prefix. Hence, the answer is -1.
+[2,1], the closest number to 1 is 2.
+[2,1,5], the closest number to 5 is 2.
+[2,1,5,8], the closest number to 8 is 5.
+[2,1,5,8,3], the closest number to 3 is 2.
+*/
+
+public class FindClosest {
+
+	@Test
+	public void example1() {
+		int[] arr = { 2, 1, 5, 8, 3 };
+		findEachClosestNumbers(arr);
+	}
+
+	@Test
+	public void example2() {
+		int[] arr = { 3, 3, 2, 4, 6, 5, 5, 1 };
+		findEachClosestNumbers(arr);
+	}
+
+	private void findEachClosestNumbers(int[] arr) {
+		PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+		
+	}
+	private void findEachClosestNumbers1(int[] arr) {
+		int[] output = new int[arr.length];
+		output[0] = -1;
+		int close, mindiff, diff;
+		for (int i = 1; i < output.length; i++) {
+			close = -1;
+			mindiff = Integer.MAX_VALUE;
+			for (int j = i - 1; j >= 0; j--) {
+				diff = Math.abs(arr[i] - arr[j]);
+				if (diff <= mindiff && diff != 0) {
+					close = (diff == mindiff) ? Math.min(close, arr[j]) : arr[j];
+					mindiff = diff;
+				}
+			}
+			output[i] = close;
+		}
+
+		System.out.println(Arrays.toString(output));
+	}
+}
