9fbf4436e3a8c2e3daeb8d68c918492614993edf
e5617111 <e5617111@MAA-LT3T6YM13.FNFIS.com>
2021-01-21 18:31:33 +0530

    Changes

--- src/sdet/week_19/weekwork/FW_ProductOfTwo.java
+++ src/sdet/week_19/weekwork/FW_ProductOfTwo.java
@@ -1,9 +1,79 @@
 package sdet.week_19.weekwork;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /*
 1) Given list of integers, find 2 numbers which matches the product
 without using map
 */
 public class FW_ProductOfTwo {
 
+	@Test
+	public void example1() {
+		int[] nums = { 4, 7, 6, 9, 3, 1, 1 };
+		int target = 18;
+		Assert.assertEquals(Arrays.toString(new int[] { 6, 3 }), Arrays.toString(productOfTwo(nums, target)));
+	}
+
+	@Test
+	public void example2() {
+		int[] nums = { 4, 9, 6, 2, 3 };
+		int target = 18;
+		Assert.assertEquals(Arrays.toString(new int[] { 9, 2 }), Arrays.toString(productOfTwo(nums, target)));
+	}
+
+	@Test
+	public void example3() {
+		int[] nums = { -1, 2, -8, -5, -2, 3 };
+		int target = 40;
+		Assert.assertEquals(Arrays.toString(new int[] { -8, -5 }), Arrays.toString(productOfTwo(nums, target)));
+	}
+
+	@Test
+	public void example4() {
+		int[] nums = { 1, 2, 4, 0, 0, 4 };
+		int target = 16;
+		Assert.assertEquals(Arrays.toString(new int[] { 4, 4 }), Arrays.toString(productOfTwo(nums, target)));
+	}
+
+	@Test
+	public void example5() {
+		int[] nums = { 1, 2, 3, 4, 5, 6 };
+		int target = 50;
+		Assert.assertEquals(Arrays.toString(new int[] {}), Arrays.toString(productOfTwo(nums, target)));
+	}
+
+	private int[] productOfTwo(int[] nums, int target) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i : nums) {
+			if (i == 0)
+				continue;
+			if (set.contains(target / i))
+				return new int[] { target / i, i };
+			else
+				set.add(i);
+		}
+		return new int[] {};
+	}
+
+	private int[] productOfTwo1(int[] nums, int target) {
+		Arrays.sort(nums);
+		int p1 = 0, p2 = nums.length - 1;
+
+		while (p1 < p2) {
+			if (nums[p1] * nums[p2] == target)
+				return new int[] { nums[p1], nums[p2] };
+			if (nums[p1] * nums[p2] > target)
+				p2--;
+			else
+				p1++;
+		}
+
+		return new int[] {};
+	}
 }
--- src/sdet/week_19/weekwork/FW_ValidateBrackets.java
+++ src/sdet/week_19/weekwork/FW_ValidateBrackets.java
@@ -1,5 +1,12 @@
 package sdet.week_19.weekwork;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /*
 2) seq of paranthesis, validate whether it is valid or not
 (){}[]<>
@@ -11,4 +18,86 @@ isPair()
 
 public class FW_ValidateBrackets {
 
+	@Test
+	public void example1() {
+		String brackets = "()";
+		Assert.assertEquals(true, isBracketsValid(brackets));
+	}
+
+	@Test
+	public void example2() {
+		String brackets = "()[]{}<>";
+		Assert.assertEquals(true, isBracketsValid(brackets));
+	}
+
+	@Test
+	public void example3() {
+		String brackets = "(]";
+		Assert.assertEquals(false, isBracketsValid(brackets));
+	}
+
+	@Test
+	public void example4() {
+		String brackets = "([)]";
+		Assert.assertEquals(false, isBracketsValid(brackets));
+	}
+
+	@Test
+	public void example5() {
+		String brackets = "{[<>]}";
+		Assert.assertEquals(true, isBracketsValid(brackets));
+	}
+
+	private boolean isBracketsValid(String brackets) {
+		Stack<Character> stack = new Stack<Character>();
+		for (char ch : brackets.toCharArray()) {
+			if (stack.isEmpty() || isOpen(ch))
+				stack.add(ch);
+			else if (isClosed(ch) && isPair("" + stack.peek() + ch))
+				stack.pop();
+			else
+				return false;
+		}
+
+		return stack.isEmpty();
+	}
+
+	private boolean isOpen(char open) {
+		Set<Character> set = new HashSet<Character>() {
+			{
+				add('{');
+				add('[');
+				add('<');
+				add('(');
+			}
+		};
+
+		return set.contains(open);
+	}
+
+	private boolean isClosed(char closed) {
+		Set<Character> set = new HashSet<Character>() {
+			{
+				add('}');
+				add(']');
+				add('>');
+				add(')');
+			}
+		};
+
+		return set.contains(closed);
+	}
+
+	private boolean isPair(String pair) {
+		Set<String> set = new HashSet<String>() {
+			{
+				add("{}");
+				add("[]");
+				add("<>");
+				add("()");
+			}
+		};
+
+		return set.contains(pair);
+	}
 }
